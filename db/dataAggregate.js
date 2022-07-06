9aa743ef6e4489912e589b9350539271e8dfd381
Michael Phan <michaelphan35@gmail.com>
2022-07-05 18:07:56 -0700

    tried another way to get notes to saved

--- db/dataAggregate.js
+++ db/dataAggregate.js
@@ -1,18 +1,35 @@
 const fs = require("fs");
-class Notes {
+
+/*module.exports = Notes => {
+    fs.readFile(".db/db.json", "utf-8", (err,data) => {
+        if (err) throw err;
+
+        const note = JSON.parse(data);
+
+        function saveData() { 
+            fs.writeFile(".db/db.json", JSON.stringify(note), err => {
+                if (err) throw err;
+                return true;
+            })
+        }
+
+    })
+}*/
+
+/* class Notes {
     getNotes() {
         fs.readFile ("./db/db.json", "utf-8", (err, data) => {
             if (err) throw error;
-            let note = JSON.parse(data);
-            function saveNotes() {
-                fs.writeFile ("./db/db.json", JSON.stringify(note), err => {
-                    return true
-                })
-            }
-        })
-    };
+             });
+             let note = JSON.parse(data);
+    saveNotes() 
+        fs.writeFile ("./db/db.json", JSON.stringify(note), err => {
+            return true
+        }
         
-    }
+        )}
+    }*/
+
 /* class Notes {
     async getNotes() {
         const data = [];
@@ -24,5 +41,3 @@ class Notes {
     async saveNotes() {
     }
 }*/
-
-module.exports = new Notes;
